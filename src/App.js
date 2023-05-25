70ce2c73d24248e1e900a82156f1a3b2cb10dd2d
GHULAM SHUBHANI <ghulam@payfinix.in>
2023-05-25 22:44:37 +0530

    add notification

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ import axios from "axios";
 import ChatBody from "./components/Chat/ChatBody";
 
 const App = () => {
+  const token = localStorage.getItem("token");
 
   // const [show, setShow] = useState(false);
   // const [notification, setNotification] = useState({ title: "", body: "" });
@@ -40,18 +41,28 @@ const App = () => {
   const notificationpremision= async ()=>{
    const permission = await Notification.requestPermission()
    if(permission==='granted'){
-     const token = await getToken(messaging, {
+     const token1 = await getToken(messaging, {
        vapidKey:
        "BK1YSNEVcw8HU87zqvSqIZIrLAegjVlT_LLIPVRycirOw5ghNJ0zH9uTT5zxceX2v04Z3E0vIIEb38Xk1QeEBRA",
       });
-      console.log("tojen", token);
+      console.log("tojen", token1);
+      const res = await axios.put(
+        "https://roomy-finder-evennode.ap-1.evennode.com/api/v1/auth/update-fcm-token",
+        {
+          fcmToken: token1,
+        },
+        { headers: { Authorization: token } }
+      );
+      console.log("res-------",res);
   // console.log("tojen", permission);
    }
    else if(permission==="denied"){
     alert("You denied for the notification")
    }
   }
-
+// if(!localStorage.getItem("token")){
+  
+// }
   useEffect(() => {
    notificationpremision()
    
--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const PrivateRoute = ({ Component, ...rest }) => {
+const PrivateRoute = ({ Component, ...rest }) => { 
+  const navigate = useNavigate();
+
+  const [token1, settoken1] = useState("")
   // Add your authentication logic here
-  const token = localStorage.getItem("token");
+ 
   const tokenExpiration = localStorage.getItem("tokenExpiration");
-
-  return token && tokenExpiration && Date.now() < parseInt(tokenExpiration) ? (
+  useEffect(() => {
+    if (token1!==""){ settoken1(localStorage.getItem("token"))}
+    else{navigate("/login")}
+    console.log("token1", token1); 
+  }, [token1])
+  
+  return token1 && tokenExpiration && Date.now() < parseInt(tokenExpiration) ? (
     <Component {...rest} />
   ) : (
     <Navigate to="/login" replace />
